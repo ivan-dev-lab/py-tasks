c8185659c8fbda79c59bbc5fd8d733437b7867d5
Ivan <ivanulanovskij143@gmail.com>
2023-02-11 21:09:54 +0500

    small bugfixes

--- pytask/pytask.py
+++ pytask/pytask.py
@@ -3,13 +3,13 @@
 
 #binary search
 from random import randint
-def binary_search():
+def binary_search() -> int:
     n = randint(1,100)
     nums = [el for el in range(1,101)]
     l, m, h = 0, 0, 0
     print(f"the desired number: {n}")
     while True:
-        print(f"the average num = {m}\n\nlist of data = {nums}")
+        #print(f"the average num = {m}\n\nlist of data = {nums}")
         l,m,h = nums[0],nums[(len(nums)//2)-1],nums[(len(nums))-1]
         if n == m:
             break
@@ -18,18 +18,23 @@ def binary_search():
         else:
             nums = [el for el in range(l,m+1)]
         
-    print(f"num finded!, the desired num = {n}")
-#binary_search()
+    return n
+#print(f"num finded!, the desired num = {binary_search()}")
 
 
 #finding the Fibonacci sequence
-def find_Fib_seq ():
-    limit = int(input("up to what number is the sequence needed: "))
+limit = int(input("up to what number is the sequence needed: "))
+def find_Fib_seq () -> list:
     i = 0
     seq = [0,1]
     i = 0
     while len(seq) < limit:
         seq.append(seq[i]+seq[i+1])
         i+=1
-    print(seq)
-#find_Fib_seq()
+    return seq
+print(f"the sequence = {find_Fib_seq()}")
+
+
+#next prime number
+def find_next_prime_number ():
+    ...
